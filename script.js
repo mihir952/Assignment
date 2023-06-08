03b528d2f2e7ad008704b20b0944cf1c9db0bc3a
Mihir Kumar <mihir.kumar@valtech.com>
2023-06-08 16:54:24 +0530

    Added some changes

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
+
+
 const axios = require('axios');
 
-async function getAllCommitStatuses(owner, repo, ref, accessToken) {
-  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/commits/${ref}/statuses`;
+const getCommits = async () => {
+  const owner = 'mihir952';
+  const repo = 'YOUR_REPO';
+  const accessToken = 'YOUR_ACCESS_TOKEN';
+
+  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/commits`;
 
   try {
     const response = await axios.get(apiUrl, {
       headers: {
-        Authorization: `Bearer ${accessToken}`
+        Authorization: `Bearer ${accessToken}`,
+        'User-Agent': 'GitHub API Client'
       }
     });
 
-    return response.data;
+    console.log(response.data);
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-// Usage example
-const owner = 'mihir952';
-const repo = 'Assignment';
-const ref = 'main'; // Branch or commit SHA
-const accessToken = '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';
+// Call the function to get commits
+getCommits();
 
-getAllCommitStatuses(owner, repo, ref, accessToken)
-  .then((commitStatuses) => {
-    console.log(commitStatuses);
-  })
-  .catch((error) => {
-    console.error(error);
-  });

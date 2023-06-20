930df800c62db15f4bbb7fae05428c9cd9b5c41e
Mihir Kumar <mihir.kumar@valtech.com>
2023-06-20 17:13:41 +0530

    Css Topics And some Animation

---
+++ Revision_CSS/p5.js
@@ -0,0 +1,15 @@
+ var container = document.getElementsByClassName("container")[1]
+// var btn = document.getElementById("btn")
+
+
+btn1.addEventListener("click",function(){
+    container.style.width = "10%";
+    console.log("Archies Singh")
+})
+
+var container1 = document.getElementsByClassName("container")[0]
+
+btn.addEventListener("click",function(){
+    container1.style.width = "80%";
+    console.log("Archies Singh")
+})
